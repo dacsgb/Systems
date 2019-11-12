901153f82eae374e9680a84d237eb511348a765a
dacsgb <dcolon@okstate.edu>
2019-11-12 12:12:39 -0600

    123

---
+++ Homework/11/problem3.m
@@ -0,0 +1,11 @@
+z = 0.5;
+
+r = linspace(1e-1,1e1,10000);
+m = -10.*log( (1-r.^2).^2 + (2*z*r).^2);
+
+figure(1)
+semilogx(freq,m)
+xlabel('w/wn')
+ylabel('m - [dB]')
+title('Normalized Bode Plot')
+grid on
